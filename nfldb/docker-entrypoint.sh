45de5003a1f9f281cb0bc0fc01f8616145dec557
Justin Labak <jtlabak@uw.edu>
2016-08-15 16:50:03 -0700

    fixing a bunch of bugs with docker build

--- nfldb/docker-entrypoint.sh
+++ nfldb/docker-entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+PORT=5432
+HOST=db
+
 if [ "$1" = 'install' ]; then
-    until nc -z db 5432
+    until nmap -Pn -p$PORT $HOST | awk "\$1 ~ /$PORT/ {print \$2}" | grep open
     do
         sleep 1
     done
@@ -12,7 +15,9 @@ if [ "$1" = 'install' ]; then
         && PGPASSWORD=nfldb psql -h db -U nfldb < nfldb.sql \
         && PGPASSWORD=nfldb psql -h db -U nfldb -c "INSERT INTO team (team_id, city, name) VALUES ('LA', 'Los Angeles', 'Rams')" \
         && PGPASSWORD=nfldb psql -h db -U nfldb -c "UPDATE team SET team_id = 'JAX' WHERE team_id = 'JAC'" \
-        && sed -i "/JAC/    ['JAX', 'Jacksonville', 'Jaguars', 'Jacksonville Jaguars', 'JAC'],/" /usr/lib/python2.7/site-packages/nfldb/team.py \
-        && python /usr/lib/python2.7/site-packages/nflgame/update_sched.py --rebuild --year 2015 --phase POST
+        && sed -i 's/.*JAC.*/    [\x27JAX\x27, \x27Jacksonville\x27, \x27Jaguars\x27, \x27Jacksonville Jaguars\x27, \x27JAC\x27],/' /usr/lib/python2.7/site-packages/nfldb/team.py \
+        && python /usr/lib/python2.7/site-packages/nflgame/update_sched.py --rebuild --year 2015 --phase POST \
         && nfldb-update
+elif [ "$1" = 'update' ]; then
+    nfldb-update
 fi
