cc1545d0c9362efabf77559dd3c554abde83b3b5
Justin Labak <jtlabak@uw.edu>
2016-08-15 13:30:34 -0700

    trying a better compose

--- nfldb/docker-entrypoint.sh
+++ nfldb/docker-entrypoint.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-curl -o nfldb.sql.zip http://burntsushi.net/stuff/nfldb/nfldb.sql.zip \
-    && unzip nfldb.sql.zip \
-    && PGPASSWORD=nfldb psql -h db -U nfldb < nfldb.sql \
-    && python /usr/lib/python2.7/site-packages/nflgame/update_sched.py --rebuild --year 2015 --phase POST
-    && nfldb-update
+if [ "$1" = 'install' ]; then
+    until nc -z db 5432
+    do
+        sleep 1
+    done
+    sleep 1
 
-
-# insert into team (team_id, city, name) values ('LA', 'Los Angeles', 'Rams');
-# update team set team_id = JAX where team_id = JAC
-# change /usr/lib/python2.7/site-packages/nfldb/team.py JAX -> JAC, JAC -> JAX
+    curl -o nfldb.sql.zip http://burntsushi.net/stuff/nfldb/nfldb.sql.zip \
+        && unzip nfldb.sql.zip \
+        && PGPASSWORD=nfldb psql -h db -U nfldb < nfldb.sql \
+        && PGPASSWORD=nfldb psql -h db -U nfldb -c "INSERT INTO team (team_id, city, name) VALUES ('LA', 'Los Angeles', 'Rams')" \
+        && PGPASSWORD=nfldb psql -h db -U nfldb -c "UPDATE team SET team_id = 'JAX' WHERE team_id = 'JAC'" \
+        && sed -i "/JAC/    ['JAX', 'Jacksonville', 'Jaguars', 'Jacksonville Jaguars', 'JAC'],/" /usr/lib/python2.7/site-packages/nfldb/team.py \
+        && python /usr/lib/python2.7/site-packages/nflgame/update_sched.py --rebuild --year 2015 --phase POST
+        && nfldb-update
+fi
